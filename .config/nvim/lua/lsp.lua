5e02ba8979f4a684d6f6a44713ba925e8454e42c
Radovan Beler <beler.radovan@gmail.com>
2023-12-11 22:08:58 +0100

    Improve shortcuts

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,6 +1,7 @@
 -- LSP Keybindings
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+vim.keymap.set('n', 'gr', vim.lsp.buf.rename, {})
 
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup({})
