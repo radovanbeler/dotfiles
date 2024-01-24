0febecac5d2bc420feec86b13466601da9ef11b7
Radovan Beler <beler.radovan@gmail.com>
2024-01-24 11:59:49 +0100

    Create autocommand to define LSP keybinding only after LSP server attaches

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,17 +1,23 @@
-vim.keymap.set("n", "gr", vim.lsp.buf.rename, {})
-vim.keymap.set("n", "gdd", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "gdD", vim.lsp.buf.declaration, {})
-vim.keymap.set("n", "gdh", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "gdf", vim.diagnostic.open_float, {})
-vim.keymap.set("n", "gda", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "gdn", vim.diagnostic.goto_next, {})
-vim.keymap.set("n", "gdp", vim.diagnostic.goto_prev, {})
-
 local lspconfig = require("lspconfig")
+
 lspconfig.pyright.setup({})
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(args)
+		local opts = { buffer = args.buf }
+		vim.keymap.set("n", "gr", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "gdd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gdD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gdh", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gdf", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "gda", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gdn", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "gdp", vim.diagnostic.goto_prev, opts)
+	end,
+})
+
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local lspconfig = require("lspconfig")
 local servers = { "clangd", "pyright" }
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
