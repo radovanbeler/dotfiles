328c267c947d489d6badedb3c9085fdc4544d84d
Radovan Beler <beler.radovan@gmail.com>
2024-02-26 14:05:53 +0100

    Configure C formatter

--- .config/nvim/lua/nvim-conform.lua
+++ .config/nvim/lua/nvim-conform.lua
@@ -1,10 +1,15 @@
 require("conform").setup({
 	formatters_by_ft = {
 		python = { "isort", "ruff_format" },
+		c = { "clang_format" },
 		lua = { "stylua" },
 		yaml = { "yamlfmt" },
 	},
-
+	formatters = {
+		clang_format = {
+			prepend_args = { "-style=file" },
+		},
+	},
 	format_on_save = {
 		timeout_ms = 500,
 		lsp_fallback = false,
