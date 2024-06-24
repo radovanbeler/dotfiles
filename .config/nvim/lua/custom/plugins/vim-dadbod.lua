a09d8754dd2a1620e33f3a1691d1bed89eaa2e8c
Radovan Beler <beler.radovan@gmail.com>
2024-06-24 18:01:51 +0200

    Make working with vim-dadbod-ui easier

--- .config/nvim/lua/custom/plugins/vim-dadbod.lua
+++ .config/nvim/lua/custom/plugins/vim-dadbod.lua
@@ -14,7 +14,9 @@ return {
             "DBUIFindBuffer",
         },
         init = function()
+            vim.keymap.set("n", "<Leader>d", "<CMD>DBUIToggle<CR>")
             vim.g.db_ui_use_nerd_fonts = 1
         end,
+        config = function() end,
     },
 }
