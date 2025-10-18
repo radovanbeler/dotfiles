ff17721f53968236cfed11f937442fb8a4717d4e
Radovan Beler <beler.radovan@gmail.com>
2025-10-18 20:39:33 +0200

    Move netrw config to ftplugin

---
+++ config/.config/nvim/after/ftplugin/netrw.lua
@@ -0,0 +1,45 @@
+vim.g.netrw_keepdir = 1
+vim.g.netrw_localcopydircmd = "cp -r"
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "netrw",
+	callback = function()
+		vim.opt_local.number = true
+		vim.opt_local.relativenumber = true
+	end,
+})
+
+local show_marked_files = function()
+	local buf = vim.api.nvim_create_buf(false, true)
+
+	local marked_files = vim.fn["netrw#Expose"]("netrwmarkfilelist")
+	if type(marked_files) == "string" then
+		marked_files = { marked_files[1] }
+	end
+
+	table.insert(marked_files, string.format("Press 'q' to continue"))
+
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, marked_files)
+
+	local width = vim.o.columns
+	local height = #marked_files
+	vim.api.nvim_open_win(buf, true, {
+		relative = "editor",
+		width = width,
+		height = height,
+		row = vim.o.lines - height,
+		col = vim.o.columns - width,
+		style = "minimal",
+		border = "none",
+	})
+
+	vim.api.nvim_buf_set_option(buf, "buftype", "nofile")
+	vim.api.nvim_buf_set_option(buf, "modifiable", false)
+	vim.api.nvim_buf_set_option(buf, "readonly", true)
+
+	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":q!<CR>", { noremap = true, silent = true })
+end
+
+vim.keymap.set("n", "ml", show_marked_files, { noremap = true, buffer = true })
+vim.keymap.set("n", "l", "<CR>", { remap = true, buffer = true })
+vim.keymap.set("n", "h", "-", { remap = true, buffer = true })
--- config/.config/nvim/lua/keymap.lua
+++ config/.config/nvim/lua/keymap.lua
@@ -4,38 +4,6 @@ vim.g.mapleader = " "
 -- Open Netrw
 vim.keymap.set("n", "<Leader>pf", vim.cmd.Ex)
 
--- Show marked files in Netrw
-vim.keymap.set("n", "ml", function()
-	local buf = vim.api.nvim_create_buf(false, true)
-
-	local marked_files = vim.fn["netrw#Expose"]("netrwmarkfilelist")
-	if type(marked_files) == "string" then
-		marked_files = { marked_files[1] }
-	end
-
-	table.insert(marked_files, string.format("Press 'q' to continue"))
-
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, marked_files)
-
-	local width = vim.o.columns
-	local height = #marked_files
-	vim.api.nvim_open_win(buf, true, {
-		relative = "editor",
-		width = width,
-		height = height,
-		row = vim.o.lines - height,
-		col = vim.o.columns - width,
-		style = "minimal",
-		border = "none",
-	})
-
-	vim.api.nvim_buf_set_option(buf, "buftype", "nofile")
-	vim.api.nvim_buf_set_option(buf, "modifiable", false)
-	vim.api.nvim_buf_set_option(buf, "readonly", true)
-
-	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":q!<CR>", { noremap = true, silent = true })
-end)
-
 -- Save file
 vim.keymap.set("n", "<Leader>s", "<CMD>:w<CR>")
 
