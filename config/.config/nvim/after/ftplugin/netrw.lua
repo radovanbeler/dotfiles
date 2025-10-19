b33d21f0cbf7563f29a6a3ed16dc6c6773b76ca1
Radovan Beler <beler.radovan@gmail.com>
2025-10-19 12:12:00 +0200

    Highlight exit text when listing marked files

--- config/.config/nvim/after/ftplugin/netrw.lua
+++ config/.config/nvim/after/ftplugin/netrw.lua
@@ -21,6 +21,9 @@ local show_marked_files = function()
 
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, marked_files)
 
+	vim.api.nvim_set_hl(0, "exitline", { fg = "#f6c177" })
+	vim.api.nvim_buf_add_highlight(buf, -1, "exitline", #marked_files - 1, 0, -1)
+
 	local width = vim.o.columns
 	local height = #marked_files
 	vim.api.nvim_open_win(buf, true, {
