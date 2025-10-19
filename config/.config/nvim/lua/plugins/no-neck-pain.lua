f2ab143cf1503a97903817f353b464d3c4812783
Radovan Beler <beler.radovan@gmail.com>
2025-10-19 12:13:03 +0200

    Add no-neck-pain.nvim plugin

---
+++ config/.config/nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,12 @@
+return {
+	"shortcuts/no-neck-pain.nvim",
+	tag = "v2.5.0",
+	config = function()
+		require("no-neck-pain").setup({
+			width = 125,
+			mappings = {
+				enabled = true,
+			},
+		})
+	end,
+}
