a8a6fc7d7f378805a1e9d7808fdc4b2d8dc3893f
Radovan Beler <beler.radovan@gmail.com>
2025-12-07 18:07:46 +0100

    Add some useful mini.nvim plugins

--- config/.config/nvim/lua/plugins/mini.lua
+++ config/.config/nvim/lua/plugins/mini.lua
@@ -1,7 +1,26 @@
 return {
-	"nvim-mini/mini.align",
-	version = false,
-	config = function()
-		require("mini.align").setup()
-	end,
+	{
+		"nvim-mini/mini.align",
+		version = false,
+		config = function()
+			require("mini.align").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.trailspace",
+		version = false,
+		config = function()
+			local trailspace = require("mini.trailspace")
+
+			trailspace.setup()
+
+			vim.keymap.set("n", "<leader>tt", function()
+				trailspace.trim()
+			end)
+
+			vim.keymap.set("n", "<leader>tl", function()
+				trailspace.trim_last_lines()
+			end)
+		end,
+	},
 }
