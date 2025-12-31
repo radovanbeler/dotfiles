b357934cd7ed10b35a28802b955715fbcdcf78fc
Radovan Beler <beler.radovan@gmail.com>
2025-12-31 09:57:14 +0100

    Migrate treesitter

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,91 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
-	priority = 110,
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		"nvim-treesitter/nvim-treesitter-refactor",
-	},
 	config = function()
-		local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
-		vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-		vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-		vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
-		vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-		vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-		vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
-
-		require("nvim-treesitter.configs").setup({
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					-- set to `false` to disable one of the mappings
-					init_selection = "gnn",
-					node_incremental = "grn",
-					scope_incremental = "grc",
-					node_decremental = "grm",
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["if"] = "@function.inner",
-						["af"] = "@function.outer",
-						["ic"] = "@class.inner",
-						["ac"] = "@class.outer",
-					},
-					-- 'v' -> charwise, 'V' -> linewise, <c-v> -> blockwise
-					selection_modes = {
-						["@function.inner"] = "V",
-						["@function.outer"] = "V",
-						["@class.inner"] = "V",
-						["@class.outer"] = "V",
-					},
-					include_surrounding_whitespace = false,
-				},
-				swap = {
-					enable = true,
-					swap_next = {
-						["gpn"] = "@parameter.inner",
-						["gmn"] = "@function.outer",
-					},
-					swap_previous = {
-						["gpp"] = "@parameter.inner",
-						["gmp"] = "@function.outer",
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]c"] = "@class.outer",
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["]C"] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-					},
-					goto_next = {},
-					goto_previous = {},
-				},
-			},
-			refactor = {
-				highlight_definitions = {
-					enable = false,
-					clear_on_cursor_move = false,
-				},
-			},
+		-- Start treesitter for every filetype
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
 		})
 	end,
 }
