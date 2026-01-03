8fd157a0ec791b198f2d701954640fa9185a09ba
Radovan Beler <beler.radovan@gmail.com>
2026-01-03 16:01:14 +0100

    Add support for treesitter textobjects

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,38 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		local ts = require("nvim-treesitter")
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = ts.get_installed(),
-			callback = function()
-				vim.treesitter.start()
-			end,
-		})
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local ts = require("nvim-treesitter")
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = ts.get_installed(),
+				callback = function()
+					vim.treesitter.start()
+				end,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		lazy = false,
+		branch = "main",
+		config = function()
+			local select = require("nvim-treesitter-textobjects.select")
+			vim.keymap.set({ "x", "o" }, "if", function()
+				select.select_textobject("@function.inner")
+			end)
+			vim.keymap.set({ "x", "o" }, "af", function()
+				select.select_textobject("@function.outer")
+			end)
+
+			local swap = require("nvim-treesitter-textobjects.swap")
+			vim.keymap.set("n", "gpn", function()
+				swap.swap_next("@parameter.inner")
+			end)
+			vim.keymap.set("n", "gpp", function()
+				swap.swap_previous("@parameter.inner")
+			end)
+		end,
+	},
 }
