6b6002b00770bd785b29fc5ad5cc83c3bfd5cd26
Radovan Beler <beler.radovan@gmail.com>
2026-01-05 08:52:04 +0100

    Enable detail toggle in oil

--- config/.config/nvim/lua/plugins/oil.lua
+++ config/.config/nvim/lua/plugins/oil.lua
@@ -3,7 +3,26 @@ return {
 	lazy = false,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("oil").setup()
+		local oil = require("oil")
+
+		local detail = false
+		local toggle_detail = function()
+			detail = not detail
+			if detail then
+				oil.set_columns({ "icon", "permissions", "user", "group", "size", "mtime" })
+			else
+				oil.set_columns({ "icon" })
+			end
+		end
+
+		oil.setup({
+			keymaps = {
+				["gd"] = {
+					callback = toggle_detail,
+				},
+			},
+		})
+
 		vim.keymap.set("n", "-", vim.cmd.Oil)
 	end,
 }
